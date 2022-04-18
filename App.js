8b85163ee3e9dce6685790a8ac0c0fa88ac85380
unknown <cmotek1122@gmail.com>
2022-04-18 14:05:49 -0500

    androidsplashscreen

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import MainContainer from './components/MainContainer';
 
+
+import { useEffect } from 'react';
+
 export default function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [])
+
+
+
   return (
     <MainContainer/>
   );
