c3dc55ac5c14122fe99f74719572c2b9fad19308
unknown <cmotek1122@gmail.com>
2022-04-29 15:22:31 -0500

    logins

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 
-const CustomInput = () => {
+const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
     return (
         <View style={styles.container}>
-            <TextInput placeholder="placeholder" style={styles.input} />
+            <TextInput
+            value={value}
+            onChangeText={setValue}
+            placeholder={placeholder} 
+            style={styles.input}
+            secureTextEntry={secureTextEntry} />
         </View>
     );
 };
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,21 +1,80 @@
-import React from 'react';
-import { View, Text, StyleSheet, useWindowDimensions } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
 import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
 
 const SignInScreen = () => {
-    return (
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
+    const onSignInPressed = () => {
+        console.warn('Sign in');
+    };
+
+    const onForgotPasswordPressed = () => {
+        console.warn('Forgot Password?');
+    };
+
+    const onSignInFacebook = () => {
+        console.warn('Sign in with Facebook');
+    };
+    const onSignInGoogle = () => {
+        console.warn('Sign in with Google');
+    };
+    const onSignInApple = () => {
+        console.warn('Sign in with Apple');
+    };
+    const onSignUpPressed = () => {
+        console.warn('Sign up!');
+    };
+    
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.root}>
-            <CustomInput />
+            <CustomInput 
+             placeholder="Username"
+             value={username}
+             setValue={setUsername}/>
+            <CustomInput 
+             placeholder="Password"
+             value={password}
+             setValue={setPassword}
+             secureTextEntry={true}/>
+            <CustomButton
+             text="Sign In"
+             onPress={onSignInPressed}/>
+            <CustomButton 
+             text="Forgot Password?"
+             onPress={onForgotPasswordPressed} 
+             type="TERTIARY"/>
+            <CustomButton
+             text="Sign In with Facebook"
+             onPress={onSignInFacebook}
+             bgColor="#E7EAF4"
+             fgColor="#4765A9"/>
+            <CustomButton
+             text="Sign In with Google"
+             onPress={onSignInGoogle}
+             bgColor="#FAE9EA"
+             fgColor="#DD4D44"/>
+            <CustomButton
+             text="Sign In with Apple"
+             onPress={onSignInApple}
+             bgColor="#e3e3e3"
+             fgColor="#363636"/>
+            <CustomButton 
+             text="Don't have an account? Create one!"
+             onPress={onSignUpPressed} 
+             type="TERTIARY"/>
         </View>
-    )
-}
+        </ScrollView>
+    );
+};
 
 const styles = StyleSheet.create({
     root: {
         alignItems: 'center',
         padding: 20,
-        maxHeight: 200, 
     },
 });
 
